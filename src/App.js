eeb684373f2ff85891726925167d64c6788102c8
Vitalii MV <agronom9913@gmail.com>
2023-07-27 17:00:33 +0300

    2023.07.27 17-00

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/freelance-portfolio-react-proect-ukr" element={<Home />} />
+          <Route path="/freelance_portfolio_react_ukr" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/project/:id" element={<Project />} />
